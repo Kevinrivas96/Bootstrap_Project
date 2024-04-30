090238890dc2eabfe6ddc532947b8884197cf5f5
Rodrigo Lopez <134024353+rlopezlara@users.noreply.github.com>
2024-04-30 11:39:54 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+// Button Alert
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible text-center fw-bold" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Hello World!', 'success')
+  })
+}
+// Form setting 
+
+function handleSubmit(event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+  // Redirect to the home page (Replace 'home.html' with the actual URL of your home page)
+  window.location.href = 'index.html';
+}
+
+// Function to handle form reset
+function handleReset() {
+  // Reset the form (Replace 'contact-form' with the actual ID of your form)
+  document.getElementById('contact-form').reset();
+}
+// PopOvers Button
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
